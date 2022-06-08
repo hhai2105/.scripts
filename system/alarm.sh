e5302ba58a6adaf4cdf063f49e855742afd2afde
Hai Nguyen <hhai2105@gmail.com>
2022-06-09 06:56:21 +0700

    create alarm script

---
+++ system/alarm.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env zsh
+vlc --loop /mnt/sda1/Music/ZUTOMAYO/Imawa\ Imade\ Chikaiwa\ Emide/1\ -\ Kan\ Saete\ Kuyashiiwa.flac
+
---
+++ system/setupAlarm.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env zsh
+youtube-dl --extract-audio "https://www.youtube.com/watch?v=77S7NhRoBc"
+mv ~/Popular Alarm Clock Sound Effect-77S70NhRoBc.m4a ~/Music
