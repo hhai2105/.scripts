77a8aecb08b724088ac6192a402f4df090961471
Hai <hhai2105@gmail.com>
2024-03-30 03:13:23 +0000

    update drive location

--- system/disk_read_only.sh
+++ system/disk_read_only.sh
@@ -1,3 +1,3 @@
-sudo umount -f /dev/nvme0n1p5
-sudo ntfsfix /dev/nvme0n1p5 # where x is HDD and X is drive number, in my case it was /dev/sda1
-sudo mount /dev/nvme0n1p5 /mnt/sda1
+sudo umount -f /dev/nvme1n1p2
+sudo ntfsfix /dev/nvme1n1p2 # where x is HDD and X is drive number, in my case it was /dev/sda1
+sudo mount /dev/nvme1n1p2 /mnt/sda1
